98e9cc743eb755d8e39463fb2472f4a63127f633
alex leon <alex@leon.st>
2018-03-13 14:58:16 +1100

    Release 1.0.0

---
+++ index.js
@@ -0,0 +1,49 @@
+// convert git:// form url to github URL, e.g.,
+// git://github.com/bcoe/foo.git
+// https://github.com/bcoe/foo.
+function githubUrlFromGit (url, opts) {
+  try {
+    var urlNoExt = url.replace(/\.git(#.*)?$/, '')
+    var snippetMatch = snippetRe(opts).exec(urlNoExt)
+
+    if (snippetMatch) {
+      return 'https://' + snippetMatch[1] + '/snippets/' + snippetMatch[2]
+    }
+
+    var match = re(opts).exec(urlNoExt)
+    return 'https://' + match[1] + '/' + match[2]
+  } catch (_err) {
+    // ignore
+  }
+}
+
+var protocolRe = /^(?:https?:\/\/|git:\/\/|git\+ssh:\/\/|git\+https:\/\/)?(?:[^@]+@)?/
+
+// generate the git:// parsing regex
+// with options, e.g., the ability
+// to specify multiple GHE domains.
+function re (opts) {
+  opts = opts || {}
+  // whitelist of URLs that should be treated as GitHub repos.
+  var baseUrls = ['bitbucket.org'].concat(opts.extraBaseUrls || [])
+  // build regex from whitelist.
+  return new RegExp(
+    protocolRe.source +
+    '(' + baseUrls.join('|') + ')' +
+    /(?::\/?|\/)([^/]+\/[^/]+?)$/.source
+  )
+}
+
+function snippetRe (opts) {
+  opts = opts || {}
+  // whitelist of URLs that should be treated as GitHub repos.
+  var baseUrls = ['bitbucket.org'].concat(opts.extraBaseUrls || [])
+  // build regex from whitelist.
+  return new RegExp(
+    protocolRe.source +
+    '(' + baseUrls.join('|') + ')' +
+    /(?::\/?|\/)snippets\/([^/]+\/[^/]+)\/[^/]+$/.source
+  )
+}
+
+module.exports = githubUrlFromGit
---
+++ test.js
@@ -0,0 +1,79 @@
+/* global describe, it */
+
+var parse = require('./')
+var assert = require('better-assert')
+
+describe('parse(url)', function () {
+  it('should support git://*', function () {
+    var url = 'git@bitbucket.org:alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should support git://*.git', function () {
+    var url = 'git://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should support https://*', function () {
+    var url = 'https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should support https://*.git', function () {
+    var url = 'https://alex@bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should return undefined on failure', function () {
+    var url = 'git://bitbucket.org/alex-e-leon/.git'
+    assert(parse(url) == null)
+  })
+
+  it('should parse git@bitbucket.org:/alex-e-leon/node-bitbucket-url-from-git.git', function () {
+    var url = 'git@bitbucket.org:/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should parse git@github.com:bcoe/thumbd.git#2.7.0', function () {
+    var url = 'git@bitbucket.org:alex-e-leon/node-bitbucket-url-from-git.git#2.7.0'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should parse git+https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git', function () {
+    var url = 'git+https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should parse git+ssh://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git', function () {
+    var url = 'git+ssh://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(url).should.eql('https://bitbucket.org/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  // snippet urls.
+
+  it('should parse git@ snippet urls', function () {
+    var url = 'git@bitbucket.org:snippets/alex-e-leon/nepk84/a-snippet.git'
+    parse(url).should.equal('https://bitbucket.org/snippets/alex-e-leon/nepk84')
+  })
+
+  it('should parse https://gist urls', function () {
+    var url = 'https://alex@bitbucket.org/snippets/alex-e-leon/nepk84/a-snippet.git'
+    parse(url).should.equal('https://bitbucket.org/snippets/alex-e-leon/nepk84')
+  })
+
+  // Handle arbitrary GitHub Enterprise domains.
+
+  it('should parse parse extra bitbucket enterprise urls provided', function () {
+    var url = 'git://bitbucket.example.com/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(
+      url, {extraBaseUrls: ['bitbucket.example.com']}
+    ).should.equal('https://bitbucket.example.com/alex-e-leon/node-bitbucket-url-from-git')
+  })
+
+  it('should parse bitbucket enterprise urls with multiple subdomains', function () {
+    var url = 'git://bitbucket.internal.example.com/alex-e-leon/node-bitbucket-url-from-git.git'
+    parse(
+      url, {extraBaseUrls: ['bitbucket.internal.example.com']}
+    ).should.equal('https://bitbucket.internal.example.com/alex-e-leon/node-bitbucket-url-from-git')
+  })
+})
